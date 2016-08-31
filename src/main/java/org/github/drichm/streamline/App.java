e3a69ed791569638063ee24e2b0cb482719e5146
David Richmond <djr667@hotmail.com>
2016-08-31 18:07:31 +0200

    Commit test - project structure only

---
+++ src/main/java/org/github/drichm/streamline/App.java
@@ -0,0 +1,13 @@
+package org.github.drichm.streamline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    { 
+        System.out.println( "Hello World!" );
+    }
+}
